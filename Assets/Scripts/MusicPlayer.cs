e774fbec22abe1d9db9b6f95747e54b8be46ef92
José Manuel <josemac95@gmail.com>
2019-08-21 20:02:18 +0200

    81. Playing music between scenes

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+	// Tiempo de carga
+	float loadTime = 2f;
+
+	// Se ejecuta antes de Start
+	void Awake()
+	{
+		// Para que no se pierda la música en otras escenas
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start()
+	{
+		Invoke("LoadFirstScene", loadTime);
+	}
+
+	private void LoadFirstScene()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
