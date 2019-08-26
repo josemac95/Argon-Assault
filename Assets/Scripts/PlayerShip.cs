c73133d69decbf64c94ff390063645bd9234a93c
José Manuel <josemac95@gmail.com>
2019-08-26 20:08:35 +0200

    84. Using cross platform input

---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerShip : MonoBehaviour
+{
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		print(horizontalThrow);
+	}
+}
