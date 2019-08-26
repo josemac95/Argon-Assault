3733638317fc6d9f8ebeaa6561f639bbe1490269
José Manuel <josemac95@gmail.com>
2019-08-26 21:03:43 +0200

    87. Understanding roll, pitch and yaw

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -5,10 +5,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerShip : MonoBehaviour
 {
-	// Velocidad máxima horizontal de la nave (m/s)
-	[Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+	// Velocidad máxima de la nave (m/s)
+	[Tooltip("In ms^-1")] [SerializeField] float speed = 5f;
 	// Limite horizontal de movimiento (m)
 	[Tooltip("In m")] [SerializeField] float xRange = 4f;
+	// Limite vertical de movimiento (m)
+	[Tooltip("In m")] [SerializeField] float yRange = 2.25f;
 
 	void Start()
 	{
@@ -17,16 +19,46 @@ public class PlayerShip : MonoBehaviour
 
 	void Update()
 	{
+		// Movimiento de la nave
+		MoveShip();
+	}
+
+	// Movimiento de la nave en la posición local
+	private void MoveShip()
+	{
+		// Posición horizontal
+		float xPos = CalculateXPos();
+		// Posición vertical
+		float yPos = CalculateYPos();
+		// Movimiento en la posición local (relativa al padre = cámara)
+		transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+	}
+
+	// Posición local en el eje horizontal
+	private float CalculateXPos()
+	{
 		// Impulso horizontal (valor de -1 a 1)
 		// Depende de la sensibilidad y la gravedad
 		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		// Desplazamiento para este frame
-		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		// Desplazamiento horizontal para este frame
+		float xOffset = xThrow * speed * Time.deltaTime;
 		// Posición local en el eje horizontal
 		float rawXPos = transform.localPosition.x + xOffset;
 		// Posición local en el eje horizontal con el limite horizontal
-		float xPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-		// Movimiento horizontal en la posición local (relativa al padre = cámara)
-		transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+		return Mathf.Clamp(rawXPos, -xRange, xRange);
+	}
+
+	// Posición local en el eje vertical
+	private float CalculateYPos()
+	{
+		// Impulso vertical (valor de -1 a 1)
+		// Depende de la sensibilidad y la gravedad
+		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		// Desplazamiento vertical para este frame
+		float yOffset = yThrow * speed * Time.deltaTime;
+		// Posición local en el eje vertical
+		float rawYPos = transform.localPosition.y + yOffset;
+		// Posición local en el eje vertical con el limite vertical
+		return Mathf.Clamp(rawYPos, -yRange, yRange);
 	}
 }
