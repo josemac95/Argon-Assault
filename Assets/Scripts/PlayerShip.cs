48ef1a90c5b362dd01dde5e9a9443683e7725618
José Manuel <josemac95@gmail.com>
2019-08-27 12:49:42 +0200

    89. Rotate ship with position and throw

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -10,7 +10,20 @@ public class PlayerShip : MonoBehaviour
 	// Limite horizontal de movimiento (m)
 	[Tooltip("In m")] [SerializeField] float xRange = 4f;
 	// Limite vertical de movimiento (m)
-	[Tooltip("In m")] [SerializeField] float yRange = 2.25f;
+	[Tooltip("In m")] [SerializeField] float yRange = 2.5f;
+	// Factor de cabeceo respecto a la posición en Y
+	[SerializeField] float positionPitchFactor = -8f;
+	// Factor de cabeceo respecto al impuslo vertical
+	[SerializeField] float yThrowPitchFactor = -10f;
+	// Factor de guiñada respecto a la posición en X
+	[SerializeField] float positionYawFactor = 7f;
+	// Factor de alabeo respecto al impuslo horizontal
+	[SerializeField] float xThrowRollFactor = -20f;
+
+	// Impulso horizontal de la nave
+	float xThrow;
+	// Impulso vertical de la nave
+	float yThrow;
 
 	void Start()
 	{
@@ -41,7 +54,7 @@ public class PlayerShip : MonoBehaviour
 	{
 		// Impulso horizontal (valor de -1 a 1)
 		// Depende de la sensibilidad y la gravedad
-		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
 		// Desplazamiento horizontal para este frame
 		float xOffset = xThrow * speed * Time.deltaTime;
 		// Posición local en el eje horizontal
@@ -55,7 +68,7 @@ public class PlayerShip : MonoBehaviour
 	{
 		// Impulso vertical (valor de -1 a 1)
 		// Depende de la sensibilidad y la gravedad
-		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 		// Desplazamiento vertical para este frame
 		float yOffset = yThrow * speed * Time.deltaTime;
 		// Posición local en el eje vertical
@@ -67,8 +80,13 @@ public class PlayerShip : MonoBehaviour
 	// Movimiento de rotación de la nave
 	private void ProcessRotation()
 	{
+		// x = cabeceo (vertical)
+		float pitch = transform.localPosition.y * positionPitchFactor + yThrow * yThrowPitchFactor;
+		// y = guiñada (horziontal)
+		float yaw = transform.localPosition.x * positionYawFactor;
+		// z = alabeo (rotación)
+		float roll = xThrow * xThrowRollFactor;
 		// Rotación local
-		// x, y, z = pitch, yaw, roll = cabeceo (vertical), guiñada (horziontal), alabeo (rotación)
-		transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
 	}
 }
