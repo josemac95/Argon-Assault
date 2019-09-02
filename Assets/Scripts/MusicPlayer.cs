7fdcdacac680c847c838cb49c443ac2df856e902
José Manuel <josemac95@gmail.com>
2019-09-02 16:24:39 +0200

    95. Project tidying checklist

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,27 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-	// Tiempo de carga
-	float loadTime = 2f;
-
 	// Se ejecuta antes de Start
 	void Awake()
 	{
 		// Para que no se pierda la música en otras escenas
 		DontDestroyOnLoad(gameObject);
-	}
-
-	void Start()
-	{
-		Invoke("LoadFirstScene", loadTime);
-	}
-
-	private void LoadFirstScene()
-	{
-		SceneManager.LoadScene(1);
 	}
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	// Tiempo de carga
+	float loadTime = 2f;
+
+	void Start()
+	{
+		Invoke("LoadFirstScene", loadTime);
+	}
+
+	private void LoadFirstScene()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
