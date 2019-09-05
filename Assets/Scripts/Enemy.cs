ebf09ba65e5d14dd1411d3a0617401f75ddc4420
José Manuel <josemac95@gmail.com>
2019-09-05 12:57:37 +0200

    101. Making scripts add components

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+	void Start()
+	{
+		// Añade el componente via script
+		// Por si se actualiza el Asset Pack
+		AddNonTriggerBoxCollider();
+	}
+
+	// Añade el collider (No trigger)
+	private void AddNonTriggerBoxCollider()
+	{
+		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+		// Por defecto es así, pero es mejor asegurarse
+		// Porque es importante para el funcionamiento
+		boxCollider.isTrigger = false;
+	}
+
 	// Si choca una partícula con el objeto
 	void OnParticleCollision(GameObject other)
 	{
