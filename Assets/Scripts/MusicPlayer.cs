5c4faa398e4fadb9117321f44a988c2735e389f7
José Manuel <josemac95@gmail.com>
2019-09-05 12:16:29 +0200

    99. Singleton pattern without statics

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Singleton sin statics
 public class MusicPlayer : MonoBehaviour
 {
 	// Se ejecuta antes de Start
 	void Awake()
 	{
-		// Para que no se pierda la música en otras escenas
-		DontDestroyOnLoad(gameObject);
+		// Encuentra los MusicPlayer
+		int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+		// Si no es el primero
+		if (numMusicPlayers > 1)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			// Para que no se pierda la música en otras escenas
+			DontDestroyOnLoad(gameObject);
+		}
 	}
 }
